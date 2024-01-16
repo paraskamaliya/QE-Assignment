bd9046db9e62b9c9532e2db160fca7fa67d65f53
Paras <paraskamaliya5@gmail.com>
2024-01-16 21:44:14 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const options = {
         },
         servers: [
             {
-                "url": "http://localhost:8080"
+                "url": "http://localhost:5000"
             }
         ]
     },
