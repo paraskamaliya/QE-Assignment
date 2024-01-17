0a8bc6c8c707bb2f4ab86a1e8d82747857de68f5
Paras <paraskamaliya5@gmail.com>
2024-01-17 08:30:29 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const options = {
         },
         servers: [
             {
-                "url": "http://localhost:5000"
+                "url": "https://lms-gr4j.onrender.com/"
             }
         ]
     },
